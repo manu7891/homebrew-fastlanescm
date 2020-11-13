63baec8ccc48024ad54da2aae1dafcf82fc06557
Emmanuel DARNIS <emmanuel.darnis@netatmo.com>
2020-11-13 20:48:03 +0100

    Add formula

---
+++ Formula/fastlane.rb
@@ -0,0 +1,42 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Fastlane < Formula
+  desc "🚀 The easiest way to automate building and releasing your iOS and Android apps"
+  homepage "https://fastlane.tools"
+  url "https://github.com/manu7891/fastlane/archive/netatmo-1.0.0.tar.gz"
+  sha256 "06579ac3b8ea0a3aef6b3d522d92724812a10c419a2a8048dac6fde12b7bdf49"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
+
+    system "gem", "build", "fastlane.gemspec"
+    system "gem", "install", "fastlane-#{version}.gem", "--no-document"
+
+    (bin/"fastlane").write <<~EOS
+      #!/bin/bash
+      export PATH="#{Formula["ruby"].opt_bin}:#{libexec}/bin:$PATH"
+      export FASTLANE_INSTALLED_VIA_HOMEBREW="true"
+      GEM_HOME="#{libexec}" GEM_PATH="#{libexec}" \\
+        exec "#{libexec}/bin/fastlane" "$@"
+    EOS
+    chmod "+x", bin/"fastlane"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test fastlane`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
